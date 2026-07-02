1ce77b0fc62c15dd9bc1bc1680b45dc026e306fb
Akash Babu <168814859+akashbabuv@users.noreply.github.com>
2026-07-02 21:00:27 +0530

    add code header documentation

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,29 +1,11 @@
+/*
+Author: Akash Babu
+Date: 02-07-2026
+Project: LED Blink using Arduino
+Description:
+Blink an LED with adjustable speed using a potentiometer
+and display the blink count on the Serial Monitor.
+*/
+
 int blinkCount = 0;
 int potPin = A0;
-
-void setup() {
-
-  pinMode(13, OUTPUT);
-
-  Serial.begin(9600);
-
-}
-
-void loop() {
-
-  int sensorValue = analogRead(potPin);
-
-  int delayTime = map(sensorValue, 0, 1023, 100, 1000);
-
-  digitalWrite(13, HIGH);
-  delay(delayTime);
-
-  digitalWrite(13, LOW);
-  delay(delayTime);
-
-  blinkCount++;
-
-  Serial.print("Blink Count: ");
-  Serial.println(blinkCount);
-
-}
